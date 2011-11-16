b094af2da0daddadc09d681b0e020337eb4a8915
jazzyb <json.barnes@gmail.com>
2011-11-15 22:58:35 -0600

    Converts moves.c to ANSI C.

--- src/moves.c
+++ src/moves.c
@@ -13,8 +13,13 @@ uint64_t fc_uint64 (const char *str)
 	x2 -= 'a';
 	y1 -= '1';
 	y2 -= '1';
-	return ((UINT64_C(1)) << (y1 * 8) + x1) |
-	       ((UINT64_C(1)) << (y2 * 8) + x2);
+
+/* Redefine UINT64_C to be something that plays nicer with the C89 standard */
+#undef UINT64_C
+#define UINT64_C(x) ((uint64_t)x)
+
+	return ((UINT64_C(1)) << ((y1 * 8) + x1)) |
+	       ((UINT64_C(1)) << ((y2 * 8) + x2));
 }
 
 void fc_move_copy (fc_move_t *dst, fc_move_t *src)
@@ -56,11 +61,13 @@ int fc_mlist_init (fc_mlist_t *list, int size)
 
 int fc_mlist_resize (fc_mlist_t *list, int new_size)
 {
+	fc_move_t *ok;
+
 	if (new_size < list->index) {
 		return 0;
 	}
 
-	fc_move_t *ok = realloc(list->moves, new_size * sizeof(fc_move_t));
+	ok = realloc(list->moves, new_size * sizeof(fc_move_t));
 	if (!ok) {
 		return 0;
 	}
@@ -91,13 +98,15 @@ int fc_mlist_append (fc_mlist_t *list, fc_player_t player, fc_piece_t piece,
 
 int fc_mlist_copy (fc_mlist_t *dst, fc_mlist_t *src)
 {
+	int i;
+
 	if (dst->size < src->index) {
 		if (!fc_mlist_resize(dst, src->size)) {
 			return 0;
 		}
 	}
 
-	for (int i = 0; i < src->index; i++) {
+	for (i = 0; i < src->index; i++) {
 		fc_move_copy(dst->moves + i, src->moves + i);
 		dst->index += 1;
 	}
@@ -107,7 +116,7 @@ int fc_mlist_copy (fc_mlist_t *dst, fc_mlist_t *src)
 
 int fc_mlist_cat (fc_mlist_t *dst, fc_mlist_t *src)
 {
-	int resize_flag = 0;
+	int i, resize_flag = 0;
 
 	while (dst->index + src->index > dst->size) {
 		resize_flag = 1;
@@ -117,7 +126,7 @@ int fc_mlist_cat (fc_mlist_t *dst, fc_mlist_t *src)
 		return 0;
 	}
 
-	for (int i = 0; i < src->index; i++) {
+	for (i = 0; i < src->index; i++) {
 		fc_move_copy(dst->moves + dst->index, src->moves + i);
 		dst->index += 1;
 	}
